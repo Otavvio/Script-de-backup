cb73b650cf44a186d5177c435f764eab167762df
OTAVIO FIORAVANTE NASCIMENTO <otavio.fnascimento1@senacsp.edu.br>
2024-10-21 16:29:04 -0300

    Versão 1.0

---
+++ backup.bat
@@ -0,0 +1,4 @@
+mkdir Backup
+copy /windows/*.txt \Users\wallace.wfsantos\Documents\script-backup 
+copy /windows/System32/*.log \Users\wallace.wfsantos\Documents\script-backup 
+copy /windows/System32/*.txt \Users\wallace.wfsantos\Documents\script-backup 
